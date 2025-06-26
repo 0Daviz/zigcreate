d0e3b72236b8eb94caf033f16b5018acb471ffce
Daviz <88402486+0Daviz@users.noreply.github.com>
2025-06-26 22:58:08 +0200

    Update templates.zig

--- src/templates.zig
+++ src/templates.zig
@@ -3,8 +3,8 @@ const std = @import("std");
 pub const MAIN_FILE_CONTENT =
     \\const std = @import("std");
     \\
-    \\pub fn main() !void {
-    \\    std.debug.print("Hello, {s}!\\n", .{"world"});
+    \\pub fn main() void {
+    \\    std.debug.print("Hello, world!\n", .{});
     \\}
 ;
 
